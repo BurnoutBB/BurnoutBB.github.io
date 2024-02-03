ebfdd4a7112fe8ff2153cd504039109198863b0d
kacper210305 <85195574+BurnoutBB@users.noreply.github.com>
2024-02-03 16:45:48 +0100

    naprawilemprzewijaniewchuj

--- przewijanie.js
+++ przewijanie.js
@@ -1,30 +1,17 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
-  });
-document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(function(link) {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        var targetId = this.getAttribute('href').substring(1);
-        var targetElement = document.getElementById(targetId);
-        var highlightedElement = document.querySelector('.highlight');
-        if (highlightedElement) {
-          highlightedElement.classList.remove('highlight');
-        }
-        if (targetElement) {
-          targetElement.classList.add('highlight');
-          targetElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest'
-          });
-          window.scrollBy(0, -50);
-          setTimeout(function() {
-            targetElement.classList.remove('highlight');
-          }, 2000);
-        }
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+      setTimeout(function() {
+        var offset = 50;
+        window.scrollBy(0, -offset);
+      }, 0);
+      setTimeout(function() {
+        section.classList.add('highlight');
+      }, 0);
+      section.addEventListener('transitionend', function removeHighlight() {
+        section.classList.remove('highlight');
+        section.removeEventListener('transitionend', removeHighlight);
       });
-    });
-  });
-  
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
--- spis.js
+++ spis.js
@@ -1,4 +1,9 @@
 function toggleSpis() {
     var trescSpisu = document.getElementById("trescSpisu");
     trescSpisu.style.display = (trescSpisu.style.display === "block") ? "none" : "block";
-}
+  }
+
+  function hideSpis() {
+    var trescSpisu = document.getElementById("trescSpisu");
+    trescSpisu.style.display = "none";
+  }
