7f6162b98f382fc24511d8e69b6a4cd1727bfd9a
BurnoutBB <katkusiokos@gmail.com>
2024-02-27 15:51:35 +0100

    Fix jakis

--- przewijanie_js.js
+++ przewijanie_js.js
@@ -6,10 +6,10 @@ function scrollToSection(sectionId) {
         window.scrollBy(0, -offset);
       }, 0);
       setTimeout(function() {
-        section.classList.add('highlight_sql');
+        section.classList.add('highlight_js');
       }, 0);
       section.addEventListener('transitionend', function removeHighlight() {
-        section.classList.remove('highlight_sql');
+        section.classList.remove('highlight_js');
         section.removeEventListener('transitionend', removeHighlight);
       });
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
